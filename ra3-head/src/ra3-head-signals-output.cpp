bfe141e19338674fe4350637c4bde856d38469ba
maisvendoo <maisvendoo@gmail.com>
2021-10-13 16:54:38 +0300

    Поправлена стрелка 24 вольта

--- ra3-head/src/ra3-head-signals-output.cpp
+++ ra3-head/src/ra3-head-signals-output.cpp
@@ -13,7 +13,7 @@ void RA3HeadMotor::stepSignalsOutput(double t, double dt)
     analogSignal[SIG_BUTTON_OFF] = static_cast<float>(!tumbler[BUTTON_PWR_OFF].getState());
 
     analogSignal[STRELKA_VOLTMETER110] = static_cast<float>(Ucc / 150.0);
-    analogSignal[STRELKA_VOLTMETER24] = static_cast<float>(0);//bat24->getVoltage() / 30.0);
+    //analogSignal[STRELKA_VOLTMETER24] = static_cast<float>(0);//bat24->getVoltage() / 30.0);
 
     // Кнопки пуска и остановки дизелей
     analogSignal[SIG_BUTTON_START] = static_cast<float>(tumbler[BUTTON_START].getState());
