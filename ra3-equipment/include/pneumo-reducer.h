ebcea12b5f44933596053708f79ffb4be932260e
maisvendoo <maisvendoo@gmail.com>
2021-10-18 16:50:32 +0300

    Заготовки под КРУ-091 и редуктор

--- ra3-equipment/include/pneumo-reducer.h
+++ ra3-equipment/include/pneumo-reducer.h
@@ -16,6 +16,15 @@ public:
 
 private:
 
+    /// Уставка давления на выходе редуктора
+    double p_ref;
+
+    /// Объем рабочей полости на выходе
+    double V_work;
+
+    /// Давление на входе редуктора
+    double p_in;
+
     void preStep(state_vector_t &Y, double t);
 
     void ode_system(const state_vector_t &Y, state_vector_t &dYdt, double t);
