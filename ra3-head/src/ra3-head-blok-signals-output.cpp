9f8aeeb97c2cefb8f694885b2e588252d7628c10
maisvendoo <maisvendoo@gmail.com>
2021-10-20 20:10:40 +0300

    Обновлена модель с исправленной рамой окна

--- ra3-head/src/ra3-head-blok-signals-output.cpp
+++ ra3-head/src/ra3-head-blok-signals-output.cpp
@@ -8,5 +8,5 @@ void RA3HeadMotor::blokSignalsOutput(double t, double dt)
     analogSignal[BLOK_REVERS] = static_cast<float>(mpsu->getOutputData().getReversorDir());
     analogSignal[BLOK_TM_PRESS] = static_cast<float>(pTM);
     analogSignal[BLOK_TC_PRESS] = static_cast<float>(brake_mech[FWD_TROLLEY]->getBrakeCylinderPressure());
-    analogSignal[BLOK_VELOCITY] = static_cast<float>(velocity * Physics::kmh);
+    analogSignal[BLOK_VELOCITY] = static_cast<float>(qAbs(velocity * Physics::kmh));
 }
