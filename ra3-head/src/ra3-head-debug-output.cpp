96dc2479e729a6cda75138f7ebec4ef3ff5b2783
maisvendoo <maisvendoo@gmail.com>
2021-10-20 10:02:55 +0300

    Добавлен конфиг редуктора в заднюю секцию. Работают тормоза по всему поезду

--- ra3-head/src/ra3-head-debug-output.cpp
+++ ra3-head/src/ra3-head-debug-output.cpp
@@ -7,7 +7,7 @@ void RA3HeadMotor::debugOutput(double t, double dt)
 {
     Q_UNUSED(dt)
 
-    DebugMsg = QString("t: %1 PM: %2 3P: %3 P1: %4 P2: %5 ER: %6 TM: %7")
+    DebugMsg = QString("t: %1 PM: %2 3P: %3 P1: %4 P2: %5 ER: %6 TM: %7 TC1: %8 TC2 %9")
             .arg(t, 10, 'f', 1)
             .arg(main_res->getPressure(), 4, 'f', 2)
             .arg(aux_res->getPressure(), 4, 'f', 2)
@@ -15,5 +15,7 @@ void RA3HeadMotor::debugOutput(double t, double dt)
             .arg(brake_mech[BWD_TROLLEY]->getParkingBrakePressure(), 4, 'f', 2)
             .arg(kru->getEqReservoirPressure(), 4, 'f', 2)
             .arg(pTM, 4, 'f', 2)
+            .arg(brake_mech[FWD_TROLLEY]->getBrakeCylinderPressure(), 4, 'f', 2)
+            .arg(brake_mech[BWD_TROLLEY]->getBrakeCylinderPressure(), 4, 'f', 2)
             + kru->getDebugMsg();
 }
