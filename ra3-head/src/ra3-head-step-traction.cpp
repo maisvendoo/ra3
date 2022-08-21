392e4c76eb0d24df9622590a28723e18c3f8eddc
Mikhail Arepev <arepmm@gmail.com>
2022-08-21 11:54:38 +0300

    Успешная работа реверса на развёрнутом вагоне
    Также исправлен звук при попытке избыточного переключения реверса

--- ra3-head/src/ra3-head-step-traction.cpp
+++ ra3-head/src/ra3-head-step-traction.cpp
@@ -24,7 +24,7 @@ void RA3HeadMotor::stepTraction(double t, double dt)
                                      (!mpsu->getOutputData().motion_disable) &&
                                       epk->isTractionAllowed());
 
-        hydro_trans->setRefReversState(static_cast<int>(forward_inputs[SME_REVERS_HANDLE]));
+        hydro_trans->setRefReversState(orient * static_cast<int>(forward_inputs[SME_REVERS_HANDLE]));
         hydro_trans->setBrakeMode(mpsu->getOutputData().hydro_brake_ON2);
     }
 
@@ -33,14 +33,6 @@ void RA3HeadMotor::stepTraction(double t, double dt)
     hydro_trans->setOmegaOutput(ip * wheel_omega[0]);
     hydro_trans->step(t, dt);
 
-    if (is_active)
-    {
-        Q_a[1] = ip * hydro_trans->getOutputTorque() / 2.0;
-        Q_a[2] = ip * hydro_trans->getOutputTorque() / 2.0;
-    }
-    else
-    {
-        Q_a[3] = ip * hydro_trans->getOutputTorque() / 2.0;
-        Q_a[4] = ip * hydro_trans->getOutputTorque() / 2.0;
-    }
+    Q_a[1] = ip * hydro_trans->getOutputTorque() / 2.0;
+    Q_a[2] = ip * hydro_trans->getOutputTorque() / 2.0;
 }
