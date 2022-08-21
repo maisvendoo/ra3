1d1bbca246eb3cd284d9883eae3d064651fd5059
Mikhail Arepev <arepmm@gmail.com>
2022-08-21 19:28:03 +0300

    Упорядочены сигналы СМЕ

--- ra3-equipment/include/ra3-sme-signals.h
+++ ra3-equipment/include/ra3-sme-signals.h
@@ -6,45 +6,49 @@
 //------------------------------------------------------------------------------
 enum
 {
-    SME_BWD_POWER_ON = 0,
-    SME_FWD_POWER_ON = 1,
-    SME_BWD_FUEL_PUMP = 2,
-    SME_BWD_FUEL_PRESS = 3,
-    SME_BWD_OIL_PRESS = 4,
-    SME_BWD_STARTER_ON = 5,
-    SME_BWD_OMEGA = 6,
-    SME_BWD_FUEL_VALVE_OPEN = 7,
-    SME_BWD_DISEL_STARTED = 8,
-    SME_BWD_COMPRESSOR = 9,
-    SME_BWD_GENERATOR = 10,
-    SME_BWD_FUEL_PUMP_ON = 11,
-    SME_PARKING_BRAKE_ON = 12,
-    SME_PARKING_BRAKE_STATE = 13,
-    SME_REF_BRAKE_LEVEL = 14,
-    SME_BRAKE_RELEASE = 15,
-    SME_EPK_STATE = 16,
-    SME_IS_KM_TRACTION = 17,
-    SME_DISEL_FREQ = 18,
-    SME_REVERS_HANDLE = 19,
-    SME_REVERS_STATE = 20,
-    SME_BWD_BC1 = 21,
-    SME_BWD_BC2 = 22,
-    SME_BWD_BRAKE_LEVEL = 23,
-    SME_IS_KM_BRAKE = 24,
-    SME_IS_KM_ZERO = 25,
-    SME_KM_TRACTION_LEVEL = 26,
-    SME_KM_BRAKE_LEVEL = 27,
-    SME_IS_EMERGENCY_BRAKE = 28,
-    SME_BWD_CAN = 29,
-    SME_NO_ACTIVE = 30,
+    // Общие сигналы для межвагонных связей
+    SME_FWD_CHARGE_VOLTAGE = 1,
+    SME_BWD_CHARGE_VOLTAGE = 2,
+    SME_FWD_PM_PRESSURE = 3,
+    SME_BWD_PM_PRESSURE = 4,
 
-    SME_FWD_CHARGE_VOLTAGE = 31,
-    SME_BWD_CHARGE_VOLTAGE = 32,
+    // Общие сигналы с ведущей секции
+    SME_BWD_POWER_ON = 10,
+    SME_REVERS_HANDLE = 11,
+    SME_IS_KM_ZERO = 12,
+    SME_IS_KM_TRACTION = 13,
+    SME_IS_KM_BRAKE = 14,
+    SME_KM_TRACTION_LEVEL = 15,
+    SME_DISEL_FREQ = 16,
+    SME_KM_BRAKE_LEVEL = 17,
+    SME_REF_BRAKE_LEVEL = 18,
+    SME_REF_BRAKE_LEVEL_EPB = 19,
+    SME_BRAKE_RELEASE = 20,
+    SME_PARKING_BRAKE_ON = 21,
+    SME_IS_EMERGENCY_BRAKE = 22,
+    SME_EPK_STATE = 23,
+    SME_NO_ACTIVE = 24,
 
-    SME_FWD_PM_PRESSURE = 33,
-    SME_BWD_PM_PRESSURE = 34,
+    // Сигналы с ведущей секции к ведомой секции
+    SME_BWD_FUEL_PUMP = 40,
+    SME_BWD_FUEL_VALVE_OPEN = 41,
+    SME_BWD_STARTER_ON = 42,
+    SME_BWD_DISEL_STARTED = 43,
 
-    SME_REF_BRAKE_LEVEL_EPB = 35
+    // Сигналы с ведомой секции к ведущей секции
+    SME_BWD_OIL_PRESS = 45,
+    SME_BWD_FUEL_PRESS = 46,
+    SME_BWD_FUEL_PUMP_ON = 47,
+    SME_BWD_OMEGA = 48,
+    SME_BWD_GENERATOR = 49,
+    SME_BWD_COMPRESSOR = 50,
+    SME_REVERS_STATE = 51,
+    SME_BWD_BRAKE_LEVEL = 52,
+    SME_BWD_BC1 = 53,
+    SME_BWD_BC2 = 54,
+    SME_PARKING_BRAKE_STATE = 55,
+    SME_BWD_CAN = 56
+
 };
 
 #endif // RA3_SME_SIGNALS_H
--- ra3-head/src/ra3-head-step-sme-signals-output.cpp
+++ ra3-head/src/ra3-head-step-sme-signals-output.cpp
@@ -10,85 +10,6 @@ void RA3HeadMotor::stepSMESignalsOutput(double t, double dt)
     Q_UNUSED(t)
     Q_UNUSED(dt)
 
-    // Сигнал включения контактора "Бортсеть" на ведомую секцию
-    backward_outputs[SME_BWD_POWER_ON] = static_cast<float>(KM_power->getContactState(2));
-
-    // Сигнал включения топливного насоса на ведомом дизеле
-    backward_outputs[SME_BWD_FUEL_PUMP] = static_cast<float>(mpsu->getOutputData().is_fuel_pump2_ON);
-
-    // Сигнал включения стартера на ведомом дизеле
-    backward_outputs[SME_BWD_STARTER_ON] = static_cast<float>(mpsu->getOutputData().is_starter2_ON);
-
-    // Сигнал открытия топливного клапана на ведомом дизеле
-    backward_outputs[SME_BWD_FUEL_VALVE_OPEN] = static_cast<float>(mpsu->getOutputData().is_fuel_valve2_open);
-
-    // Сигнал запуска генератора на ведомой секции
-    forward_outputs[SME_BWD_GENERATOR] = static_cast<float>(generator->isActive());
-
-    // Передаем давление масла в ведущую секцию
-    forward_outputs[SME_BWD_OIL_PRESS] = static_cast<float>(disel->getOilPressure());
-
-    // Передаем обороты дизеля на ведущую секцию
-    forward_outputs[SME_BWD_OMEGA] = static_cast<float>(disel->getShaftFreq());
-
-    // Передаем сигнал включения топливного насоса на ведущую секцию
-    forward_outputs[SME_BWD_FUEL_PUMP_ON] = static_cast<float>(fuel_pump->isStarted());
-
-    // Передаем давление топлива в ведущую секцию
-    forward_outputs[SME_BWD_FUEL_PRESS] = static_cast<float>(fuel_pump->getFuelPressure());
-
-    // Сигнал запуска дизеля для включения ПСН в ведущей секции
-    backward_outputs[SME_BWD_DISEL_STARTED] = static_cast<float>(mpsu->getOutputData().is_disel2_started);
-
-    // Сигнал запуска компрессора на ведущую секцию
-    forward_outputs[SME_BWD_COMPRESSOR] = static_cast<float>(motor_compr->isPowered());
-
-    // Команда управления стояночным тормозом на ведущую секцию
-    backward_outputs[SME_PARKING_BRAKE_ON] = static_cast<float>(tumbler[SWITCH_PARKING_BRAKE].getState());
-
-    // Сигнал состояния стояночного тормоза на ведущую секцию
-    forward_outputs[SME_PARKING_BRAKE_STATE] = static_cast<float>(brake_module->isParkingBraked());
-
-    // Заданный уровень торможения ЭПТ на ведомую секцию
-    backward_outputs[SME_REF_BRAKE_LEVEL] = km->getBrakeLevel();
-
-    // Сигнал отпуска тормозов (КЭБ) на ведомую секцию или промежуточный вагон
-    backward_outputs[SME_BRAKE_RELEASE] = static_cast<float>(mpsu->getOutputData().release_PB1);
-
-    backward_outputs[SME_REF_BRAKE_LEVEL_EPB] = static_cast<float>(mpsu->getOutputData().brake_ref_level_EPB);
-
-    // Питание удерживающей катушки ЭПК
-    backward_outputs[SME_EPK_STATE] = static_cast<float>(blok->getEPKstate());
-
-
-    backward_outputs[SME_DISEL_FREQ] = static_cast<float>(mpsu->getOutputData().n_ref);
-
-    backward_outputs[SME_REVERS_HANDLE] = km->getReversHandlePos();
-
-    forward_outputs[SME_REVERS_STATE] = hydro_trans->getReversState();
-
-    forward_outputs[SME_BWD_BC1] = brake_mech[FWD_TROLLEY]->getBrakeCylinderPressure();
-    forward_outputs[SME_BWD_BC2] = brake_mech[BWD_TROLLEY]->getBrakeCylinderPressure();
-    forward_outputs[SME_BWD_BRAKE_LEVEL] = hydro_trans->getBrakeLevel();
-
-    // Состояние блок-контактов КМ
-    backward_outputs[SME_IS_KM_ZERO] = static_cast<float>(km->isZero());
-    backward_outputs[SME_IS_KM_TRACTION] = static_cast<float>(km->isTraction());
-    backward_outputs[SME_IS_KM_BRAKE] = static_cast<float>(km->isBrake());
-
-    // Уровень тяги и торможения с КМ для задней секции
-    backward_outputs[SME_KM_BRAKE_LEVEL] = km->getBrakeLevel();
-    backward_outputs[SME_KM_TRACTION_LEVEL] = km->getTractionLevel();
-
-    backward_outputs[SME_IS_EMERGENCY_BRAKE] = static_cast<float>(
-                emerg_brake_valve->isEmergencyBrake() ||
-                km->isEmergencyBrake());
-
-    // Сигнал наличия связи по как для ведущей секции
-    forward_outputs[SME_BWD_CAN] = 1.0f;
-
-    backward_outputs[SME_NO_ACTIVE] = 1.0f;
-
     // Напряжение зарядки АКБ на промежуточный вагон
     backward_outputs[SME_FWD_CHARGE_VOLTAGE] = aux_conv->getU_110();
     forward_outputs[SME_BWD_CHARGE_VOLTAGE] = aux_conv->getU_110();
@@ -96,4 +17,93 @@ void RA3HeadMotor::stepSMESignalsOutput(double t, double dt)
     // Давление в ПМ для промежуточного вагона
     backward_outputs[SME_FWD_PM_PRESSURE] = main_res->getPressure();
     forward_outputs[SME_BWD_PM_PRESSURE] = main_res->getPressure();
+
+
+    // Сигнал включения контактора "Бортсеть" на ведомые секции
+    backward_outputs[SME_BWD_POWER_ON] = static_cast<float>(KM_power->getContactState(2));
+
+    // Сигнал позиции реверсора на ведомые секции
+    backward_outputs[SME_REVERS_HANDLE] = km->getReversHandlePos();
+
+    // Сигналы блок-контактов КМ на ведомые секции
+    backward_outputs[SME_IS_KM_ZERO] = static_cast<float>(km->isZero());
+    backward_outputs[SME_IS_KM_TRACTION] = static_cast<float>(km->isTraction());
+    backward_outputs[SME_IS_KM_BRAKE] = static_cast<float>(km->isBrake());
+
+    // Уровень тяги от КМ на ведомые секции
+    backward_outputs[SME_KM_TRACTION_LEVEL] = km->getTractionLevel();
+
+    // Частота оборотов дизеля на ведомые секции
+    backward_outputs[SME_DISEL_FREQ] = static_cast<float>(mpsu->getOutputData().n_ref);
+
+    // Уровень торможения от КМ на ведомые секции
+    backward_outputs[SME_KM_BRAKE_LEVEL] = km->getBrakeLevel();
+
+    // Заданный уровень торможения ЭПТ на ведомые секции
+    backward_outputs[SME_REF_BRAKE_LEVEL] = km->getBrakeLevel();
+    backward_outputs[SME_REF_BRAKE_LEVEL_EPB] = static_cast<float>(mpsu->getOutputData().brake_ref_level_EPB);
+
+    // Сигнал отпуска тормозов (КЭБ) на ведомые секции и промежуточные вагоны
+    backward_outputs[SME_BRAKE_RELEASE] = static_cast<float>(mpsu->getOutputData().release_PB1);
+
+    // Сигнал управления стояночным тормозом на ведомые секции
+    backward_outputs[SME_PARKING_BRAKE_ON] = static_cast<float>(tumbler[SWITCH_PARKING_BRAKE].getState());
+
+    // Сигнал экстренного торможения на ведомые секции
+    backward_outputs[SME_IS_EMERGENCY_BRAKE] = static_cast<float>(
+                emerg_brake_valve->isEmergencyBrake() ||
+                km->isEmergencyBrake());
+
+    // Сигнал наличия питания на удерживающей катушке ЭПК
+    backward_outputs[SME_EPK_STATE] = static_cast<float>(blok->getEPKstate());
+
+    // Сигнал разрешения сделать кабину активной
+    backward_outputs[SME_NO_ACTIVE] = 1.0f;
+
+
+    // Сигнал включения топливного насоса на ведомом дизеле
+    backward_outputs[SME_BWD_FUEL_PUMP] = static_cast<float>(mpsu->getOutputData().is_fuel_pump2_ON);
+
+    // Сигнал открытия топливного клапана на ведомом дизеле
+    backward_outputs[SME_BWD_FUEL_VALVE_OPEN] = static_cast<float>(mpsu->getOutputData().is_fuel_valve2_open);
+
+    // Сигнал включения стартера на ведомом дизеле
+    backward_outputs[SME_BWD_STARTER_ON] = static_cast<float>(mpsu->getOutputData().is_starter2_ON);
+
+    // Сигнал запуска дизеля для включения ПСН в ведомой секции
+    backward_outputs[SME_BWD_DISEL_STARTED] = static_cast<float>(mpsu->getOutputData().is_disel2_started);
+
+
+    // Передаем давление масла в ведущую секцию
+    forward_outputs[SME_BWD_OIL_PRESS] = static_cast<float>(disel->getOilPressure());
+
+    // Передаем давление топлива в ведущую секцию
+    forward_outputs[SME_BWD_FUEL_PRESS] = static_cast<float>(fuel_pump->getFuelPressure());
+
+    // Передаем состояние топливного насоса в ведущую секцию
+    forward_outputs[SME_BWD_FUEL_PUMP_ON] = static_cast<float>(fuel_pump->isStarted());
+
+    // Передаем обороты дизеля в ведущую секцию
+    forward_outputs[SME_BWD_OMEGA] = static_cast<float>(disel->getShaftFreq());
+
+    // Передаем состояние генератора в ведущую секцию
+    forward_outputs[SME_BWD_GENERATOR] = static_cast<float>(generator->isActive());
+
+    // Передаем состояние компрессора в ведущую секцию
+    forward_outputs[SME_BWD_COMPRESSOR] = static_cast<float>(motor_compr->isPowered());
+
+    // Передаем состояние гидропередачи в ведущую секцию
+    forward_outputs[SME_REVERS_STATE] = hydro_trans->getReversState();
+    forward_outputs[SME_BWD_BRAKE_LEVEL] = hydro_trans->getBrakeLevel();
+
+    // Передаем давление в тормозных цилиндрах в ведущую секцию
+    forward_outputs[SME_BWD_BC1] = brake_mech[FWD_TROLLEY]->getBrakeCylinderPressure();
+    forward_outputs[SME_BWD_BC2] = brake_mech[BWD_TROLLEY]->getBrakeCylinderPressure();
+
+    // Передаем состояние стояночного тормоза в ведущую секцию
+    forward_outputs[SME_PARKING_BRAKE_STATE] = static_cast<float>(brake_module->isParkingBraked());
+
+    // Сигнал наличия связи с ведущей секцией
+    forward_outputs[SME_BWD_CAN] = 1.0f;
+
 }
