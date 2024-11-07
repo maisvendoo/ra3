37d860e8cae603ba01877369dfaf51ce8e9854dd
Mikhail Arepev <arepmm@gmail.com>
2024-11-07 23:49:26 +0300

    Убрал старую модель РА-3, продолжаю структурировать сигналы на анимации новой модели

--- ra3-middle/include/ra3-middle-signals.h
+++ ra3-middle/include/ra3-middle-signals.h
@@ -9,39 +9,61 @@
 enum
 {
 /*
+    // === АНИМАЦИИ КУЗОВА ===
+    // Резерв - бортовой номер
+    //SERIAL_NUMBER = 0,
+
     // Колёсные пары
-    WHEELSET_1,
-    WHEELSET_2,
-    WHEELSET_3,
-    WHEELSET_4,
+    WHEELSET_1 = 1,
+    WHEELSET_2 = 2,
+    WHEELSET_3 = 3,
+    WHEELSET_4 = 4,
+
+    // Резерв сигналов под поворот тележек в кривых
+    //TROLLEY_FWD_ANGLE = 5,
+    //TROLLEY_BWD_ANGLE = 6,
 
     // Двери: предупреждающий сигнал, ступенька, выдвижная направляющая, створки
     // Дверь левая передняя
-    DOOR_L_FWD_WARNING_INDICATOR,
-    DOOR_L_FWD_STEP,
-    DOOR_L_FWD_SKID,
-    DOOR_L_FWD_LEFT,
-    DOOR_L_FWD_RIGHT,
-    // Дверь правая передняя
-    DOOR_R_FWD_WARNING_INDICATOR,
-    DOOR_R_FWD_STEP,
-    DOOR_R_FWD_SKID,
-    DOOR_R_FWD_LEFT,
-    DOOR_R_FWD_RIGHT,
+    DOOR_L_FWD_WARNING_INDICATOR = 7,
+    DOOR_L_FWD_STEP = 8,
+    DOOR_L_FWD_SKID = 9,
+    DOOR_L_FWD_LEFT = 10,
+    DOOR_L_FWD_RIGHT = 11,
     // Дверь левая задняя
-    DOOR_L_BWD_WARNING_INDICATOR,
-    DOOR_L_BWD_STEP,
-    DOOR_L_BWD_SKID,
-    DOOR_L_BWD_LEFT,
-    DOOR_L_BWD_RIGHT,
+    DOOR_L_BWD_WARNING_INDICATOR = 12,
+    DOOR_L_BWD_STEP = 13,
+    DOOR_L_BWD_SKID = 14,
+    DOOR_L_BWD_LEFT = 15,
+    DOOR_L_BWD_RIGHT = 16,
+    // Дверь правая передняя
+    DOOR_R_FWD_WARNING_INDICATOR = 17,
+    DOOR_R_FWD_STEP = 18,
+    DOOR_R_FWD_SKID = 19,
+    DOOR_R_FWD_LEFT = 20,
+    DOOR_R_FWD_RIGHT = 21,
     // Дверь правая задняя
-    DOOR_R_BWD_WARNING_INDICATOR,
-    DOOR_R_BWD_STEP,
-    DOOR_R_BWD_SKID,
-    DOOR_R_BWD_LEFT,
-    DOOR_R_BWD_RIGHT,
+    DOOR_R_BWD_WARNING_INDICATOR = 22,
+    DOOR_R_BWD_STEP = 23,
+    DOOR_R_BWD_SKID = 24,
+    DOOR_R_BWD_LEFT = 25,
+    DOOR_R_BWD_RIGHT = 26,
 
-    SIGNALS_SIZE
+    // Резерв сигналов под анимации поворота сцепного устройства спереди
+    //COUPLING_FWD_SHIFT = 27,
+    //COUPLING_FWD_ANGLE = 28,
+
+    // Резерв сигналов под анимации поворота сцепного устройства сзади
+    //COUPLING_BWD_SHIFT = 26,
+    //COUPLING_BWD_ANGLE = 27,
+
+    // Резерв сигналов под анимации концевых кранов тормозной магистрали
+    //ANGLECOCK_BP_FWD_HANDLE = 28,
+    //ANGLECOCK_BP_BWD_HANDLE = 29,
+
+    // Резерв сигналов под анимации концевых кранов питательной магистрали
+    //ANGLECOCK_FL_FWD_HANDLE = 30,
+    //ANGLECOCK_FL_BWD_HANDLE = 31,
 */
     // Углы поворота колесных пар
     WHEEL_1 = 194,
